877f3fb05cecdf70adc59f795c5b96360c100209
Ammar Mansab <ammar.mansab@techvention.ae>
2024-01-15 22:37:57 +0500

    fixes

--- src/app/api/blog.js
+++ src/app/api/blog.js
@@ -1,3 +1,4 @@
+"use client"
 import useSWR, { mutate } from "swr";
 import { useMemo } from "react";
 // utils
--- src/app/category/[id]/page.js
+++ src/app/category/[id]/page.js
@@ -1,16 +1,12 @@
 "use client";
-import CategoryCard from "@/components/categoryPage/categoryCard";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import styles from "./category.module.scss";
 import { Pagination } from "antd";
 import Hero from "@/components/categoryPage/Hero";
-import styles1 from "../../../components/categoryPage/category.module.scss";
-import Image from "next/image";
+
 import { useParams } from "next/navigation";
-import axios from "axios";
 import { BsSearch } from "react-icons/bs";
 import '../../../components/Home/tlp/Tlp.css'
-import { useRouter } from "next/navigation";
 
 import {
   useGetPostsByCategory
@@ -23,16 +19,12 @@ import useNavi from "@/utils/hooks/useNavi";
 const page = () => {
 
   const {handleNavigation}=useNavi();
-  const {turncateText=}
   const params = useParams();
   const { id } = params;
-  const [theindex, setTheindex] = useState(true)
   const [page, setPage] = useState(0)
   const { posts: recentPosts, postsLoading } = useGetPostsByCategory(id, page);
-  const [dummydata, setDummydata] = useState([]);
 
   let newrecentPosts = [...recentPosts, ...recentPosts]
-  // console.log(newrecentPosts, 'reccc');
 
   const API = "https://server.blog.digiunction.com";
 
@@ -40,7 +32,6 @@ const page = () => {
   const [currentPage, setCurrentPage] = useState(1);
 
   const totalPosts = newrecentPosts?.length;
-  const totalPages = Math.ceil(totalPosts / pageSize);
 
   const handlePageChange = (page) => {
     setCurrentPage(page);
@@ -55,8 +46,6 @@ const page = () => {
     return text?.length > maxLength ? `${text.slice(0, maxLength)}...` : text;
   };
 
-  const [Index, setIndex] = useState()
-  var BigIndex = 0;
   let isSpecialCard
 
   const CategoryCard = ({ post, index }) => {
@@ -74,9 +63,6 @@ const page = () => {
                   <p>{truncateText(description, 350)}</p>
                 </div>
               </div>
-              {/* <div className={` shadow-md ${styles['full-card special-card-cate']}`} >
-               
-              </div> */}
             </div>
           </>
         ) : (
