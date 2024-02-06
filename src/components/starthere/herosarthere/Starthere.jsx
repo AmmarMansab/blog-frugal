97dcbb9b140577a2867c39031121a4f1df6ce578
Ammar Mansab <ammar.mansab@techvention.ae>
2024-02-06 23:30:58 +0500

    p

--- src/components/starthere/herosarthere/Starthere.jsx
+++ src/components/starthere/herosarthere/Starthere.jsx
@@ -31,7 +31,7 @@ const Starthere = () => {
                 </div>
                 <div class=" container  max-w-80 mx-auto grid grid-cols-1 xsm:grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-2">
                     <div className="pr-2 pb-0 rounded-md sm:w-full md:w-1/1 lg:w-1/1 xl:w-1/1 flex justify-start items-start ">
-                        <img className='here-img mb-14' src='https://images.unsplash.com/photo-1682695796497-31a44224d6d6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHx8' />
+                        <img className='here-img mb-14' src='https://images.unsplash.com/photo-1682695796497-31a44224d6d6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHx8' width='500' height='333' />
                     </div>
                     <div className="pr-2 pb-0 rounded-md sm:w-full md:w-1/1 lg:w-1/1 xl:w-1/1">
                         <p className='here-p1 text-2xl mb-4 font-medium tracking-wide' >Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia sequi totam consectetur suscipit quisquam quae, </p>
