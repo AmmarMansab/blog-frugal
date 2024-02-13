95d48343b98bd9e8b2f0762d646df8720df06107
Ammar Mansab <ammar.mansab@techvention.ae>
2024-02-14 01:03:35 +0500

    message

--- src/components/common/navbar/Navbar.jsx
+++ src/components/common/navbar/Navbar.jsx
@@ -117,6 +117,7 @@ const Navbar = () => {
                           cursor: "pointer",
                         }}
                       />
+                      <a href="https://www.pinterest.com/FMFblog" target="_blank" rel="noopener noreferrer">
                       <SiPinterest
                         style={{
                           color: "black",
@@ -125,6 +126,7 @@ const Navbar = () => {
                           cursor: "pointer",
                         }}
                       />
+                      </a>
                       <FaSquareXTwitter
                         style={{
                           color: "black",
