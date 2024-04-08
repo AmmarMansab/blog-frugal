635ced0bdc0e8c27043cea8e220f679d4265d218
Ammar Mansab <ammar.mansab@techvention.ae>
2024-04-08 23:53:06 +0500

    a

--- src/components/Home/hero/H.jsx
+++ src/components/Home/hero/H.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import "./H.css"
 
 const H = () => {
   return (
-    <div>H</div>
+    <section class="hero">
+
+    <div class="hero-content">
+    
+       <h1 class="hero-title">
+            Discover the World
+       </h1>
+         
+       <h2 class="hero-subtitle">
+            We deal with the best Fashion outfits
+       </h2>
+         
+       {/* <button type="button" class="hero-button" onclick="location.href='tours.html'">
+            Search for tours &raquo;
+       </button> */}
+    
+    </div>
+
+</section>
   )
 }
 
