ed025fcd488dc6e6cfb7f9c9dc84d4eb104806f8
Ammar Mansab <ammar.mansab@techvention.ae>
2024-04-08 23:55:01 +0500

    s

--- src/components/Home/hero/H.jsx
+++ src/components/Home/hero/H.jsx
@@ -10,19 +10,20 @@ const H = () => {
        <h1 class="hero-title">
             Discover the World
        </h1>
-         
+        
        <h2 class="hero-subtitle">
             We deal with the best Fashion outfits
        </h2>
-         
-       {/* <button type="button" class="hero-button" onclick="location.href='tours.html'">
-            Search for tours &raquo;
-       </button> */}
-    
+             
     </div>
 
 </section>
   )
 }
+
+
+
+
+
 
 export default H
